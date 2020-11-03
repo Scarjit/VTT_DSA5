1ca7a0ff605de592714b29ea39ff1d1b507a2da7
Scarjit <ferdinand.linnenberg@aol.com>
2020-11-04 00:04:38 +0100

    abc

--- _code_gen/python/US/Classes/LinkPage.py
+++ _code_gen/python/US/Classes/LinkPage.py
@@ -7,4 +7,10 @@ TODO: Implement me :)
 
 class LinkPage(Page):
     def __init__(self, page: Page):
-        pass
+        self.url = page.url
+        self.contains_rules = page.contains_rules
+        self.soup = page.soup
+        self.links = page.links
+        self.sub_pages = page.sub_pages
+        self.path = page.path
+        self.data = page.data
